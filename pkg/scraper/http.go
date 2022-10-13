b712f0e00ff8f02460efa0c36f2ad4d3b1c58af9
liuyuezhong <liuyuezhong@inke.cn>
2022-10-13 12:27:17 +0800

    up

--- pkg/scraper/http.go
+++ pkg/scraper/http.go
@@ -2,8 +2,12 @@ package scraper
 
 import (
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
 	"github.com/ahuigo/requests"
+	"math"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -48,4 +52,46 @@ func RequestSession(cookies []*http.Cookie, ua string, retry int, timeout time.D
 	}
 
 	return s
+}
+
+type CookieJson struct {
+	Domain         string      `json:"domain"`
+	ExpirationDate float64     `json:"expirationDate"`
+	HostOnly       bool        `json:"hostOnly"`
+	HttpOnly       bool        `json:"httpOnly"`
+	Name           string      `json:"name"`
+	Path           string      `json:"path"`
+	SameSite       string      `json:"sameSite"`
+	Secure         bool        `json:"secure"`
+	Session        bool        `json:"session"`
+	StoreId        interface{} `json:"storeId"`
+	Value          string      `json:"value"`
+}
+
+func ReadCookieFromFile(file string) ([]*http.Cookie, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("read file %s err: %w", file, err)
+	}
+	var cookies []CookieJson
+	if err := json.Unmarshal(data, &cookies); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal err: %w", err)
+	}
+
+	ret := make([]*http.Cookie, 0, len(cookies))
+	for _, c := range cookies {
+		sec, dec := math.Modf(c.ExpirationDate)
+
+		ret = append(ret, &http.Cookie{
+			Name:     c.Name,
+			Value:    c.Value,
+			Path:     c.Path,
+			Domain:   c.Domain,
+			Expires:  time.Unix(int64(sec), int64(dec*(1e9))),
+			Secure:   c.Secure,
+			HttpOnly: c.HttpOnly,
+		})
+	}
+
+	return ret, nil
 }
